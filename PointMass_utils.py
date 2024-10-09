7afa2143f0dbf2bc39171a8717f14c5ebf955e15
SarmadMehrdad <sm9167@nyu.edu>
2024-10-08 20:57:31 -0400

    Newest AutoReg commit. AutoReg 6 incomplete

--- PointMass_utils.py
+++ PointMass_utils.py
@@ -1,3 +1,4 @@
+from calendar import c
 import crocoddyl
 import pinocchio
 from IPython.display import HTML
@@ -103,9 +104,56 @@ class Costs():
             cost += 0.5*np.sum(w_run*(self.residuals(X,U)**2))*dt
         cost += 0.5*np.sum(w_term*(self.residuals(x[-1],None)**2))
         return cost
+    
+    def traj_cost_and_feat(self, x, u, w_run, w_term, dt):
+        cost = 0
+        cum_f = np.zeros(self.nr)
+        for X, U in zip(x[:-1],u):
+            res = self.residuals(X,U)**2
+            f = 0.5*(res)
+            cum_f += f
+            cost += np.sum(w_run*f)*dt
+        res = self.residuals(x[-1],None)**2
+        f = 0.5*(res)
+        cum_f += f
+        cost += np.sum(w_term*f)
+        return cost, cum_f
+
+    def traj_cost_and_feat_modified(self, x, u, w_run, w_term, dt, mean=None, cov_inv_sqrt=None):
+        if cov_inv_sqrt is None:
+            cov_inv_sqrt = np.eye(self.nr)
+        if mean is None:
+            mean = np.zeros(self.nr)
+        cost = 0
+        cum_f = np.zeros(self.nr)
+        for X, U in zip(x[:-1],u):
+            res = self.residuals(X,U)**2
+            res_mod = res[:,None] - mean[:,None]; res_mod = cov_inv_sqrt @ res_mod; res_mod += mean[:,None]
+            f = 0.5*(np.squeeze(res_mod))
+            cum_f += f
+            cost += np.sum(w_run*f)*dt
+        res = self.residuals(x[-1],None)
+        res_mod = res[:,None] - mean[:,None]; res_mod = cov_inv_sqrt @ res_mod; res_mod += mean[:,None]
+        f = 0.5*(np.squeeze(res_mod))
+        cum_f += f
+        cost += np.sum(w_term*f)
+        return cost, cum_f
+
+
 
 def normalize(x):
-    return x/np.max(x)
+    if np.max(x) != 0.0:
+        return x/np.max(x)
+    else:
+        return x
+
+def set_merge(x,y):
+    z = []
+    for x_ in x:
+        z.append(x_)
+    for y_ in y:
+        z.append(y_)
+    return z
 
 def animatePointMass(xs, obstacles, target, sleep=50, show=False):
     print("processing the animation ... ")
@@ -152,7 +200,7 @@ def animatePointMass(xs, obstacles, target, sleep=50, show=False):
         plt.show()
     return anim
 
-def animateTraj(xs_opt, xs_set, obstacles, target, sleep=1000, animType=1):
+def animateTraj(xs_opt, xs_set, obstacles, target, set_size, sleep=1000, animType=1):
     print("processing the animation ... ")
     fig = plt.figure()
     ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
@@ -199,6 +247,22 @@ def animateTraj(xs_opt, xs_set, obstacles, target, sleep=1000, animType=1):
         time = i * sleep / 1000.0
         time_text.set_text(f"time = {time:.1f} sec")
         return ax, time_text
+    
+    def animate3(i):
+        ax.cla()
+        init()
+        if i < set_size:
+            X = xs_set[:i]
+        else:
+            X = xs_set[i-set_size:i]
+        alpha_value = 1
+        for x_ in X[:-1]:
+            ax.plot(x_[:,0], x_[:,1],color='r',alpha=(0.4*alpha_value/(len(X))))
+            alpha_value += 1
+        x_ = X[-1]; ax.plot(x_[:,0], x_[:,1],color='b',alpha=(0.4*alpha_value/(len(X))))
+        time = i * sleep / 1000.0
+        time_text.set_text(f"time = {time:.1f} sec")
+        return ax, time_text
         
     if animType == 1:
         anim = animation.FuncAnimation(
@@ -208,13 +272,17 @@ def animateTraj(xs_opt, xs_set, obstacles, target, sleep=1000, animType=1):
         anim = animation.FuncAnimation(
             fig, animate2, init_func=init, frames=len(xs_set), interval=sleep, blit=False
         )
+    elif animType == 3:
+        anim = animation.FuncAnimation(
+            fig, animate3, init_func=init, frames=len(xs_set), interval=sleep, blit=False
+        )
         
     print("... processing done")
     return anim
 
 def plot_results(x_opt, x_nopt, x_irl, obstacles, target):
     fig = plt.figure()
-    ax = plt.axes(xlim=(-2, 15), ylim=(-2, 15))
+    ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
     goal = plt.Rectangle((target[0]-0.5,target[1]-0.5),1,1,fc="g", alpha=0.7) 
     time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
     ax.add_patch(goal)
@@ -237,7 +305,7 @@ def plot_results(x_opt, x_nopt, x_irl, obstacles, target):
 
 def plot_1_traj(x, obstacles, target, label='', linemap = 'k:'):
     fig = plt.figure()
-    ax = plt.axes(xlim=(-2, 15), ylim=(-2, 15))
+    ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
     goal = plt.Rectangle((target[0]-0.5,target[1]-0.5),1,1,fc="g", alpha=0.7) 
     time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
     ax.add_patch(goal)
@@ -256,7 +324,7 @@ def plot_1_traj(x, obstacles, target, label='', linemap = 'k:'):
 
 def plot_1_set(x, obstacles, target, label='', linemap_traj = 'b', linemap_set='r:', traj_alpha = 0.5):
     fig = plt.figure()
-    ax = plt.axes(xlim=(-2, 15), ylim=(-2, 15))
+    ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
     goal = plt.Rectangle((target[0]-0.5,target[1]-0.5),1,1,fc="g", alpha=0.7) 
     time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
     ax.add_patch(goal)
@@ -278,7 +346,7 @@ def plot_1_set(x, obstacles, target, label='', linemap_traj = 'b', linemap_set='
 
 def plot_1_multiset(x, x_set, obstacles, target, label='', linemap_traj = 'g:', linemap_set='r:'):
     fig = plt.figure()
-    ax = plt.axes(xlim=(-2, 15), ylim=(-2, 15))
+    ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
     goal = plt.Rectangle((target[0]-0.5,target[1]-0.5),1,1,fc="g", alpha=0.7) 
     time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
     ax.add_patch(goal)
@@ -310,7 +378,7 @@ def check_collision(x, obs_set):
 def plot_tested_model(xs_set, obstacles, target):
     num_col = 0
     fig = plt.figure()
-    ax = plt.axes(xlim=(-2, 15), ylim=(-2, 15))
+    ax = plt.axes(xlim=(-2, 12), ylim=(-2, 12))
     goal = plt.Rectangle((target[0]-0.5,target[1]-0.5),1,1,fc="g", alpha=0.7) 
     time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
     ax.add_patch(goal)
@@ -332,7 +400,7 @@ def plot_tested_model(xs_set, obstacles, target):
             color = 'b'
         plt.plot(xs[:,0],xs[:,1], color=color, alpha=0.3)
     plt.show()
-    return num_col
+    print('Number of Collisions: {}'.format(num_col))
         
 
 def distributions(cost_set, x_set, u_set, w_run, w_term, dt):
